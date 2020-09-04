3020a7bbcc74be702d94fca8124d003fa6fb7f4c
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-04 18:35:52 +0100

    create product class constructor

--- classes.dart
+++ classes.dart
@@ -1,7 +1,15 @@
+import 'dart:html';
+
 class Product {
   String name;
   double price;
   String color;
+
+  Point(String name, double price, String color) {
+    this.name = name;
+    this.price = price;
+    this.color = color;
+  }
 }
 
 // the main function
