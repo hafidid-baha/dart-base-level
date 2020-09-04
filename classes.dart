d6f599f932f1b52fbd7ab65b8dbfbc628854bae8
hafid id-baha <hafidsoftdev@gmail.com>
2020-09-04 19:01:31 +0100

    create setter and getter

--- classes.dart
+++ classes.dart
@@ -10,6 +10,7 @@ class Product {
   }
   // getters and setters
   String get ProductInfo => this.name + " : " + this.color;
+  set setColor(String color) => this.color = color;
 }
 
 // the main function
@@ -17,5 +18,6 @@ void main() {
   var product = Product("prod", 12.0, "red");
   // get object type
   print('the object type ${product.runtimeType}');
+  product.setColor = "yellow";
   print(product.ProductInfo);
 }
